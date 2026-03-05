e5430c1e17a807fa120d99420350dc8f89f23cc2
Nivas R V <90444294+NivasRV8842@users.noreply.github.com>
2026-03-05 12:48:38 +0530

    Update database_connection.py

---
+++ utils/database_connection.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def get_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="N########2",
+        database="cricket_db"
+    )
+    return connection
+
+
+# Test connection
+if __name__ == "__main__":
+    try:
+        conn = get_connection()
+        print("✅ Database Connected Successfully!")
+        conn.close()
+    except Exception as e:
+        print("❌ Error:", e)
